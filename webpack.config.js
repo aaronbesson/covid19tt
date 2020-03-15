fa331d8e07bf50ed5fba1bd95efbd3f32744f00c
AaronB <aaronbesson@gmail.com>
2020-03-15 16:42:41 -0400

    publish

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,26 @@
-const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const path = require('path')
+const createExpoWebpackConfigAsync = require('@expo/webpack-config')
 
-module.exports = async function(env, argv) {
-  const config = await createExpoWebpackConfigAsync(env, argv);
-  // Customize the config before returning it.
-  return config;
-};
+// source: https://docs.expo.io/versions/latest/guides/customizing-webpack/
+// Expo CLI will await this method so you can optionally return a promise.
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv)
+  if (config.mode === 'development') {
+    config.devServer.compress = false
+  }
+
+  // Or prevent minimizing the bundle when you build.
+  if (config.mode === 'production') {
+    config.optimization.minimize = false
+  }
+  config.resolve = {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  }
+  config.module.rules = [{
+    // Include ts, tsx, js, and jsx files.
+    test: /\.(ts|jsx|js)x?$/,
+    // exclude: /node_modules/,
+    loader: 'babel-loader'
+  }]
+  return config
+}
